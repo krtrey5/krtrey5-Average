9defbe70558975f36609acc621ec89a722cacfab
krtrey5 <krtrey@outlook.com>
2023-02-28 01:31:02 +0000

    Average

--- src/main/java/Average.java
+++ src/main/java/Average.java
@@ -18,6 +18,14 @@ public class Average {
      * @return the average of all the values of nums.
      */
     public double getAvg(int[] nums){
-        return 0;
+        int sum = 0;
+        double div = 0;
+        for(int i = 0; i < nums.length; i++){
+            sum = sum + nums[i];
+        }
+        Double dblSum = Double.valueOf(sum);
+        
+        div = dblSum / nums.length;
+        return div;
     }
 }
